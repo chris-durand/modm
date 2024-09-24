3505a2be785cd4059679d79739b592957309dc23
Christopher Durand <christopher.durand@rwth-aachen.de>
2024-09-24 20:13:57 +0200

    fixup! [stm32] Add FDCAN support for H7 series

--- src/modm/board/nucleo_h723zg/board.hpp
+++ src/modm/board/nucleo_h723zg/board.hpp
@@ -35,6 +35,7 @@ struct SystemClock
 	// Max 550MHz
 	static constexpr uint32_t SysClk = 550_MHz;
 	static constexpr uint32_t Pll1Q = SysClk / 4;
+	static constexpr uint32_t Pll2Q = 120_MHz;
 	// Max 550MHz
 	static constexpr uint32_t Hclk = SysClk / 1; // D1CPRE
 	static constexpr uint32_t Frequency = Hclk;
@@ -76,8 +77,9 @@ struct SystemClock
 
 	static constexpr uint32_t LpUart1 = Apb4;
 
-	static constexpr uint32_t Fdcan1 = Hse;
-	static constexpr uint32_t Fdcan2 = Hse;
+	static constexpr uint32_t Fdcan1 = Pll2Q;
+	static constexpr uint32_t Fdcan2 = Pll2Q;
+	static constexpr uint32_t Fdcan3 = Pll2Q;
 
 	static constexpr uint32_t I2c1 = Apb1;
 	static constexpr uint32_t I2c2 = Apb1;
@@ -124,6 +126,19 @@ struct SystemClock
 			.pllR  = 2,		// 550 MHz / 2   = 275 MHz
 		};
 		Rcc::enablePll1(Rcc::PllSource::Hse, pllFactors1);
+
+		// Use PLL2 for FDCAN 120MHz
+		const Rcc::PllFactors pllFactors2{
+			.range = Rcc::PllInputRange::MHz1_2,
+			.pllM = 4,		//   8MHz / M=   2MHz
+			.pllN = 120,	//   2MHz * N= 240MHz
+			.pllP = 2,		// 240MHz / P= 120MHz
+			.pllQ = 2,		// 240MHz / Q= 120MHz
+			.pllR = 2,		// 240MHz / R= 120MHz
+		};
+		Rcc::enablePll2(Rcc::PllSource::ExternalClock, pllFactors2);
+		Rcc::setCanClockSource(Rcc::CanClockSource::Pll2Q);
+
 		// Use PLL3 for USB 48MHz
 		const Rcc::PllFactors pllFactors3{
 			.range = Rcc::PllInputRange::MHz4_8,
